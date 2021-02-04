6161d58ce92a3f87418031bfd3b646a14f2d2b13
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-04 14:02:59 -0600

    starting on the select component

---
+++ scripts/attractions/AttractionsSelect.js
@@ -0,0 +1,9 @@
+import { useAttractions, getAttractions} from "./AttractionsProvider.js"
+
+const eventHub = document.querySelector(".container")
+
+const contentTarget = document.querySelector(".attractionSelect")
+
+eventHub.addEventListener("change", changeEvent =>{
+    if (changeEvent.target.id)
+})
