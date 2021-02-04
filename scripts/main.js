81c75a077f360be7734466d1143f8b35fc116cad
powellmad <madsnpowell@gmail.com>
2021-02-04 11:30:12 -0600

    weather provider and preview

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,4 @@
 console.log("Welcome to the main module")
+import { useForecast } from "./weather/WeatherProvider.js"
 
-import { getParks, useParks } from "./parks/ParkProvider.js"
-
-const ParkList = () => {
-    getParks()
-        .then(() => {
-            const parks = useParks()
-            parks.forEach(element => {
-                console.log(parks.length +" "+ element.name +" "+ element.id)
-            });
-        })
-}
-
-ParkList()
+useForecast()
