7f1cd1deb1182b37e5a8aa33e5a5b8727be1a671
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-04 14:10:17 -0600

    still working on select

--- scripts/attractions/AttractionsSelect.js
+++ scripts/attractions/AttractionsSelect.js
@@ -5,5 +5,29 @@ const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".attractionSelect")
 
 eventHub.addEventListener("change", changeEvent =>{
-    if (changeEvent.target.id)
+    if (changeEvent.target.id === "attractionSelect"){
+        const customEvent = new CustomEvent("attractionChosen", {
+            detail:{
+                chosenAttraction: changeEvent.target.value
+            }
+        })
+        eventHub.dispatchEvent(customEvent)
+    }
 })
+
+
+
+
+
+
+const render = (attractionsCollection) => {
+
+    contentTarget.innerHTML = `
+        <h3>Select a Bizarre Attraction</h3>
+        <select class="dropdown" id="attractionSelect">
+            <option value="0">Please select a crime...</option>
+            ${attractionsCollection.map(attractionsObject =>`<option value="${attractionsObject.id}">${attractions.name}</option>`).join("")
+            }
+        </select>
+    `
+}
