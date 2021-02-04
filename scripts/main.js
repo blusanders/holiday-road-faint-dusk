05dea91322d8f6cd3a72be8c09c45595eba119ba
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-04 10:52:33 -0600

    added and tested attractions provider

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,17 @@
-import { getParks, useParks } from "./parks/ParkProvider.js"
 
-const ParkList = () => {
-    getParks()
-        .then(() => {
-            const parks = useParks()
-            parks.forEach(element => {
-                console.log(parks.length +" "+ element.name +" "+ element.id)
-            });
-        })
-}
+// import { getParks, useParks } from "./parks/ParkProvider.js"
+import {getAttractions, useAttractions} from "./attractions/AttractionsProvider.js"
+// const ParkList = () => {
+//     getParks()
+//         .then(() => {
+//             const parks = useParks()
+//             parks.forEach(element => {
+//                 console.log(parks.length +" "+ element.name +" "+ element.id)
+//             });
+//         })
+// }
 
-ParkList()
+// ParkList()
+
+console.log(useAttractions())
+getAttractions()
