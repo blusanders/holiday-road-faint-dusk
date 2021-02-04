5f0183d8472d75b099e43baed95d611d7d9ae982
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-02-04 11:09:15 -0600

    Added Eateries Provider Fetch call

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,20 @@
-console.log("Welcome to the main module")
+// console.log("Welcome to the main module")
 
-import { getParks, useParks } from "./parks/ParkProvider.js"
+import { getEateries, useEateries } from "./eateries/EateriesProvider.js";
+getEateries()
+useEateries()
 
-const ParkList = () => {
-    getParks()
-        .then(() => {
-            const parks = useParks()
-            parks.forEach(element => {
-                console.log(parks.length +" "+ element.name +" "+ element.id)
-            });
-        })
-}
+// import { getParks, useParks } from "./parks/ParkProvider.js"
 
-ParkList()
+// const ParkList = () => {
+//     getParks()
+//         .then(() => {
+//             const parks = useParks()
+//             parks.forEach(element => {
+//                 console.log(parks.length +" "+ element.name +" "+ element.id)
+//             });
+//         })
+// }
+
+// ParkList()
+
--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -1,11 +1,10 @@
 import ".Settings.js"
 
 let parks = [];
-
 export const useParks = () => parks.slice()
 
 export const getParks = () => {
-    const fetchKey=
+    const fetchKey="https://developer.nps.gov/api/v1/parks?limit=1000&api_key=fpZ1GrXHJMbCsrBwtjjtXi5iHCz90F3HsHJ5B4c9"
     return fetch(fetchKey)
     .then(response => response.json())
     .then(parsedParks => {
