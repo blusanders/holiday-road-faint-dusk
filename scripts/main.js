7ed122efe6fa40919ccabcca091d3f6c0fffecf9
blusanders <blusanders@yahoo.com>
2021-02-04 14:06:25 -0600

    Starting Park Select

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,11 @@
 
-import { ParksList } from "./parks/ParksProvider.js"
-
-ParksList()
-// import { getParks, useParks } from "./parks/ParkProvider.js"
 // console.log("Welcome to the main module")
-import { getParks, useParks } from "./parks/ParkProvider.js"
+
+import { ParksList } from "./parks/ParksProvider.js"
 import {getAttractions, useAttractions} from "./attractions/AttractionsProvider.js"
 import { getEateries, useEateries } from "./eateries/EateriesProvider.js";
 getEateries()
 useEateries()
 getAttractions()
-
-// const ParkList = () => {
-//     getParks()
-//         .then(() => {
-//             const parks = useParks()
-//             parks.forEach(element => {
-//                 console.log(parks.length +" "+ element.name +" "+ element.id)
-//             });
-//         })
-// }
-
-// ParkList()
-
-
-
-
-
-
-
+ParksList()
 
