ddf73654132ddd25beb782acfee79bfff7f85521
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-02-05 12:17:18 -0600

    Changes

--- scripts/main.js
+++ scripts/main.js
@@ -3,13 +3,10 @@ import { getEateries, useEateries } from "./eateries/EateriesProvider.js";
 import { AttractionSelect } from "./attractions/AttractionsSelect.js";
 import { ForecastPreview } from "./weather/WeatherPreview.js"
 import {getAttractions, useAttractions} from "./attractions/AttractionsProvider.js"
+import { EateriesSelect } from "./eateries/EateriesSelect.js";
 
 
-
-
-
-
-
+EateriesSelect()
 ForecastPreview()
 getEateries()
 useEateries()
