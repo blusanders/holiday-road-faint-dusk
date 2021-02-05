fb7f99ec3094c3c5416f38936d13f2fdaed7cd35
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-02-05 14:47:38 -0600

    Added to seetings

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,5 @@
-// export const settings = {
-// 	graphhopperKey: "",
-// 	npsKey: "",
-// 	weatherKey: ""
-// };
+export const settings = {
+	graphhopperKey: "724ca659-0cc3-4859-a1ab-7b0178172622",
+	npsKey: "fpZ1GrXHJMbCsrBwtjjtXi5iHCz90F3HsHJ5B4c9",
+	weatherKey: "1ac1d497dbfbeb2b2bcdc56759028d67"
+};
