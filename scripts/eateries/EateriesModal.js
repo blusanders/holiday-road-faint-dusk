ffbe353f186d1a330d83e9ed1d1420d18e41c0a7
Andrea Mays <theallegromusicacademy70@gmail.com>
2021-02-08 12:30:50 -0600

    Finished  Preview Section with Details Button

---
+++ scripts/eateries/EateriesModal.js
@@ -0,0 +1,8 @@
+const contentTarget = document.querySelector(".eateriesModal")
+
+import { useEateries } from "./EateriesProvider.js";
+
+export const eateriesModal = () => {
+
+    const HTMLReps 
+}
