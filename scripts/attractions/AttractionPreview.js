4dbc43078fdef3c7c7198eeb459192d24ecee833
Loganwebb1994 <loganwebb1994@gmail.com>
2021-02-07 21:40:25 -0600

    got attractions to render on DOM

--- scripts/attractions/AttractionPreview.js
+++ scripts/attractions/AttractionPreview.js
@@ -3,7 +3,7 @@ const contentTarget = document.querySelector(".attractionPreview")
 
 eventHub.addEventListener("attractionChosen", event =>{
     console.log("do it work")
-    contentTarget.innerHTML= parseInt(event.detail.chosenAttraction)
+    contentTarget.innerHTML= event.detail.chosenAttraction
 })
 
 let attractionHTMLRepresentation = ""
